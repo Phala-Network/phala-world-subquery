ad152a630aefa1e4b1b052e1bb510d34f99991aa
hashwarlock <hashwarlock@protonmail.com>
2022-07-10 16:48:58 -0500

    Update project.yaml then generate types

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,35 +1,25 @@
 import {SubstrateEvent} from "@subql/types";
-import {StarterEntity} from "../types";
-import {Balance} from "@polkadot/types/interfaces";
+import {MintedOriginOfShellNft, StartedIncubationTime, MintedShellNft} from "../types";
 
 
 export async function handleMintedOriginOfShell(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
+    const {event: {data: [rarityType, collectionId, nftId, owner, race, career, generationId]}} = event;
     //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
+    const record = await MintedOriginOfShellNft.get(event.block.block.header.hash.toString());
     await record.save();
 }
 
 export async function handleStartedIncubation(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
+    const {event: {data: [collectionId, nftId, owner, startTime, hatchTime]}} = event;
     //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
+    const record = await StartedIncubationTime.get(event.block.block.header.hash.toString());
     await record.save();
 }
 
 export async function handleMintedShell(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
+    const {event: {data: [shellCollectionId, shellNftId, rarity, career, race, generationId, originOfShellCollectionId, originOfShellNftId, owner]}} = event;
     //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
+    const record = await MintedShellNft.get(event.block.block.header.hash.toString());
     await record.save();
 }
 
---
+++ src/types/models/index.ts
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Auto-generated , DO NOT EDIT
+
+export {MintedOriginOfShellNft} from "./MintedOriginOfShellNft"
+
+export {MintedShellNft} from "./MintedShellNft"
+
+export {StartedIncubationTime} from "./StartedIncubationTime"
+
